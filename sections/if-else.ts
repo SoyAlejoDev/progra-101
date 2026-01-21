facd04fb7ac988832cb720f0b1f6cbf326045f12
SoyAlejoDev <diazperezalejandro26@gmail.com>
2026-01-21 13:48:02 -0500

    Add if-else structure demonstration in TypeScript

---
+++ sections/if-else.ts
@@ -0,0 +1,23 @@
+console.log("Inicio de programa");
+
+let isTired: boolean = true;
+
+/* if (isTired) {
+  console.log("Tomar cafe");
+} else {
+  if (!isTired) {
+    console.log("Tomar Agua");
+  }
+} */
+
+let grade: number = 55;
+
+if (grade >= 60) {
+  console.log("Aprobo el curso");
+} else if (grade >= 50) {
+  console.log("Hizo el curso pero debe mejorar");
+} else {
+  console.log("Por favor repita el curso");
+}
+
+console.log("Fin de programa");
