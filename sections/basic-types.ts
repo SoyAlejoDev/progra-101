b7e206b6c138d51809b35321973ee97e2ffa0399
SoyAlejoDev <diazperezalejandro26@gmail.com>
2026-01-21 13:12:25 -0500

    Add basic types demonstration in TypeScript

---
+++ sections/basic-types.ts
@@ -0,0 +1,28 @@
+// Strings
+console.log('-------------------strings-------------------')
+let firstName:string = "Alejo";
+let lastName:string = "Dev";
+let fullName:string = `${firstName} ${lastName}`; // Template literals
+console.log(`el nombre es ${firstName}`)
+console.log(`el apellido es ${lastName}`)
+console.log(`el nombre completo es ${fullName}`)
+console.log(`el dev mas mostro es: ${fullName}`)
+
+// Numbers
+console.log('-------------numbers-------------------')
+let number1: number = 10;
+let number2: number = 20;
+let result: number = number1 + number2;
+console.log(`el numero 1 es : ${number1}`)
+console.log(`el numero 2 es ${number2}`)
+console.log(`el resultado de la suma es: ${result}`)
+
+//booleans
+console.log('-------------booleans-------------------')
+let isActive: boolean = true
+let itRunning: boolean = false
+let hasKids: boolean= false
+console.log(`el valor de isActive es: ${isActive}`)
+console.log('isActive', isActive)
+console.log('itRunning',itRunning)
+console.log('hasKids',hasKids)
