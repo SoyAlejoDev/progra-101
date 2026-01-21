dad0f9d4dce86b3c8958f72453cc4e9ebf5e48d2
SoyAlejoDev <diazperezalejandro26@gmail.com>
2026-01-21 14:38:17 -0500

    Add demonstration of while and do-while loops in TypeScript

---
+++ sections/while-do-while.ts
@@ -0,0 +1,18 @@
+//ciclo while
+/* let gastTank: number = 50;
+while (gastTank >= 0) {
+  console.log("Gasoline level:", gastTank);
+  gastTank--;
+}
+
+console.log("Out of gasoline!");
+ */
+
+//ciclo do while
+let gastTank: number = 50;
+do {
+  console.log("Gasoline level:", gastTank);
+  gastTank--;
+} while (gastTank >= 0);
+
+console.log("Out of gasoline!");
